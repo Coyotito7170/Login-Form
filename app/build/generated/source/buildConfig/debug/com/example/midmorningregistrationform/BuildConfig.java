8b969c4781399a0e26f57f127cc550b17547e3e6
Coyotito7170 <titoedwin045@gmail.com>
2023-04-13 12:09:12 +0300

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/example/midmorningregistrationform/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.midmorningregistrationform;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.midmorningregistrationform";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
